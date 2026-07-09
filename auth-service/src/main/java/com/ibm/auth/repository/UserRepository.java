c31c30dccc729d5e3dca8d33111ffaf6a2a912f1
Rajeev Prasad <rajeev.ranjan.prasad1@ibm.com>
2026-07-09 15:22:59 +0530

    implemented the employeeid generation

--- auth-service/src/main/java/com/ibm/auth/repository/UserRepository.java
+++ auth-service/src/main/java/com/ibm/auth/repository/UserRepository.java
@@ -24,4 +24,8 @@ public interface UserRepository extends MongoRepository<User, String> {
 
     List<User> findAllByDeletedFalse();
 
+    boolean existsByEmployeeId(String employeeId);
+
+    Optional<User> findTopByOrderByEmployeeIdDesc();
+
 }
